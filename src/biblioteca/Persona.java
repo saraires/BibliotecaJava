48ae48baa411c6c4e3a6ff7083f24fd99ed7af0c
Sarai Restrepo <66284121+saraires@users.noreply.github.com>
2024-09-13 07:56:30 -0500

    First Commit

---
+++ src/biblioteca/Persona.java
@@ -0,0 +1,76 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package biblioteca;
+
+/**
+ *
+ * @author sarai
+ */
+public class Persona {
+    private String nombre;
+    private String apellido;
+    private int identificacion;
+    private String nacimiento;
+    private String sexo;
+    private String nacionalidad;
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public int getIdentificacion() {
+        return identificacion;
+    }
+
+    public void setIdentificacion(int identificacion) {
+        this.identificacion = identificacion;
+    }
+
+    public String getNacimiento() {
+        return nacimiento;
+    }
+
+    public void setNacimiento(String nacimiento) {
+        this.nacimiento = nacimiento;
+    }
+
+    public String getSexo() {
+        return sexo;
+    }
+
+    public void setSexo(String sexo) {
+        this.sexo = sexo;
+    }
+
+    public String getNacionalidad() {
+        return nacionalidad;
+    }
+
+    public void setNacionalidad(String nacionalidad) {
+        this.nacionalidad = nacionalidad;
+    }
+
+    public String getDefuncion() {
+        return defuncion;
+    }
+
+    public void setDefuncion(String defuncion) {
+        this.defuncion = defuncion;
+    }
+    private String defuncion;
+            
+}
